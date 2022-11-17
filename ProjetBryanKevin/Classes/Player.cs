c5ca4eca94a995218c9f1c95bf5f4c4824fac9f6
kevinmtn <kevin.maton@condorcet.be>
2022-11-17 15:38:44 +0100

    Add Classe
    new classe for administrator, player, user, videogame and loan

---
+++ ProjetBryanKevin/Classes/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetBryanKevin.Classes
+{
+    public class Player : User
+
+    {
+        private int credit;
+        private string pseudo;
+        private DateTime registrationDate;
+        private DateTime dateOfBirth;
+
+        public Player(int id, string userName, string password, int credit, string pseudo, DateTime registrationDate, DateTime dateOfBirth) : base(id, userName, password)
+        {
+
+            base.Id = id;
+            base.UserName = userName;
+            base.Password = password;
+            this.credit = credit;
+            this.pseudo = pseudo;
+            this.registrationDate = registrationDate;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+        public int Credit
+        {
+            get { return credit; }
+            set { credit = value; }
+        }
+        public string Pseudo
+        {
+            get { return pseudo; }
+            set { pseudo = value; }
+        }
+
+        public DateTime RegistrationDate
+        {
+            get { return registrationDate; }
+            set { registrationDate = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set{ dateOfBirth = value;}
+        }
+    }
+}
---
+++ ProjetBryanKevin/Classes/VideoGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetBryanKevin.Classes
+{
+    public class VideoGame
+    {
+        private string name;
+        private int creditCost;
+        private string console;
+
+        public VideoGame(string name, int creditCost, string console)
+        {
+            this.name = name;
+            this.creditCost = creditCost;
+            this.console = console;
+        }
+
+        public string Name { 
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int CreditCost
+        {
+            get { return this.creditCost; }
+            set { this.creditCost = value; }
+        }
+        
+        public string Console
+        {
+            get { return this.console; }
+            set { this.console = value; }
+        }
+    }
+}
