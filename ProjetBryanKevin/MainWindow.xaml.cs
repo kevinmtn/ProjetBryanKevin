55245aaa2cf4ec9ab703d51bc8e5ba441c3a7e2e
kevinmtn <kevin.maton@condorcet.be>
2022-11-26 11:12:22 +0100

    ADD factory classes + connection page
    new AbstractFactoryClasse and MSSQLFactory classe

--- ProjetBryanKevin/MainWindow.xaml.cs
+++ ProjetBryanKevin/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ProjetBryanKevin.DAO;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,69 @@ namespace ProjetBryanKevin
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            AbstractDAOFactory adf = AbstractDAOFactory.GetFactory(DAOFactoryType.MS_SQL_FACTORY);
+            String login = Pseudo.Text;
+            String password = Password.Password.ToString();
+
+            switch (ConnexionChoice.Text)
+            {
+                case "Membre":
+                    DAO<Member> memberDAO = adf.GetMemberDAO();
+                    Member memb = memberDAO.VerificationConnection(login, password);
+
+                    if (memb == null)
+                    {
+                        ErrorMessageForLogin();
+                    }
+                    else
+                    {
+                        WindowForMember wfm = new WindowForMember(memb);
+                        wfm.Show();
+                        this.Close();
+                    }
+
+                    break;
+
+                case "Trésorier":
+                    DAO<Treasurer> treasurerDAO = adf.GetTreasurerDAO();
+                    Treasurer tres = treasurerDAO.VerificationConnection(login, password);
+
+                    if (tres == null)
+                    {
+                        ErrorMessageForLogin();
+                    }
+
+                    else
+                    {
+                        WindowForTreasurer wft = new WindowForTreasurer(tres);
+                        wft.Show();
+                        this.Close();
+                    }
+
+                    break;
+
+                case "Responsable":
+                    DAO<Responsible> responsibleDAO = adf.GetResponsibleDAO();
+                    Responsible resp = responsibleDAO.VerificationConnection(login, password);
+
+                    if (resp == null)
+                    {
+                        ErrorMessageForLogin();
+                    }
+                    else
+                    {
+                        WindowForResponsible wfr = new WindowForResponsible(resp);
+                        wfr.Show();
+                        this.Close();
+                    }
+
+                    break;
+            }
+
         }
     }
 }
