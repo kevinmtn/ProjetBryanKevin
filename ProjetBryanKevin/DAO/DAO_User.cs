873148cdddf32fd03af1740a34a540ccde64761d
Bryan Marquette <bryan.marquette@condorcet.be>
2022-12-07 19:17:32 +0100

    correction coquilles

--- ProjetBryanKevin/DAO/DAO_User.cs
+++ ProjetBryanKevin/DAO/DAO_User.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjetBryanKevin.DAO
-{
+{ 
     public abstract class DAO_User<T> : DAO<T> 
     { 
         public abstract T VerificationConnection(string username, string password);
